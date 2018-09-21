dcb93e9a0ff4d14ae348379f4f68f6c3da98f287
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-09-21 09:41:33 +0200

    fix run.sh

--- run.sh
+++ run.sh
@@ -8,13 +8,15 @@ MUSER="pimcore"
 MPASS="pimcore"
 MDB="pimcore"
 
-su -u www-data /var/www/coreshop/vendor/bin/pimcore-install --admin-username admin --admin-password coreshop --mysql-host-socket $MHOST --mysql-username $MUSER --mysql-password $MPASS --mysql-database $MDB --no-interaction --ignore-existing-config --no-debug
+/var/www/coreshop/vendor/bin/pimcore-install --admin-username admin --admin-password coreshop --mysql-host-socket $MHOST --mysql-username $MUSER --mysql-password $MPASS --mysql-database $MDB --no-interaction --ignore-existing-config --no-debug
 
-su -u www-data /var/www/coreshop/bin/console cache:clear --no-warmup --no-interaction
-su -u www-data /var/www/coreshop/bin/console pimcore:bundle:enable CoreShopCoreBundle --no-interaction
-su -u www-data /var/www/coreshop/bin/console assets:install --no-interaction
-su -u www-data /var/www/coreshop/bin/console coreshop:install --no-interaction
+/var/www/coreshop/bin/console cache:clear --no-warmup --no-interaction
+/var/www/coreshop/bin/console pimcore:bundle:enable CoreShopCoreBundle --no-interaction
+/var/www/coreshop/bin/console assets:install --no-interaction
+/var/www/coreshop/bin/console coreshop:install --no-interaction
 
-su -u www-data /var/www/coreshop/bin/console coreshop:install:demo --no-interaction
+/var/www/coreshop/bin/console coreshop:install:demo --no-interaction
+
+chown -R www-data /var/www/coreshop
 
 exec $@
